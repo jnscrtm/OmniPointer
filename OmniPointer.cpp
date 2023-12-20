759fb9981a81efacd003f9f8e817a459e166986d
jnscrtm <jnsecretm@gmail.com>
2023-12-20 10:42:28 +0700

    Add project files.

---
+++ OmniPointer.cpp
@@ -0,0 +1,53 @@
+﻿#include "OmniPointer.hpp"
+
+OmniPointer::OmniPointer(OmniPointer&& var) noexcept : _ptr(var._ptr), type_hash(var.type_hash), destroyer(var.destroyer)
+{
+    var._ptr = nullptr;
+    var.type_hash = 0;
+    var.destroyer = nullptr;
+}
+
+void OmniPointer::Reset(std::nullptr_t) noexcept
+{
+    if (destroyer != nullptr)
+        destroyer(_ptr);
+
+    _ptr = nullptr;
+    type_hash = 0;
+    destroyer = nullptr;
+}
+
+void OmniPointer::Swap(OmniPointer& other) noexcept
+{
+    OmniPointer interm = std::forward<OmniPointer>(other);
+    other = std::forward<OmniPointer>(*this);
+    *this = std::forward<OmniPointer>(interm);
+}
+
+OmniPointer& OmniPointer::operator=(OmniPointer&& var) noexcept
+{
+    if (destroyer != nullptr)
+        destroyer(_ptr);
+
+    _ptr = var._ptr;
+    type_hash = var.type_hash;
+    destroyer = var.destroyer;
+
+    var._ptr = nullptr;
+    var.type_hash = 0;
+    var.destroyer = nullptr;
+
+    return *this;
+}
+
+OmniPointer& OmniPointer::operator=(std::nullptr_t) noexcept
+{
+    Reset(nullptr);
+    return *this;
+}
+
+OmniPointer::~OmniPointer() noexcept
+{
+    if (destroyer != nullptr)
+        destroyer(_ptr);
+}
---
+++ OmniPointer.hpp
@@ -0,0 +1,127 @@
+﻿#pragma once
+
+#include <cstddef>
+#include <stdexcept>
+#include <type_traits>
+#include <typeinfo>
+
+/// @brief A pointer that can point to basically any type of object. Each `OmniPointer` shall uniquely own an object, if any.
+/// @author Jean-Valentin Auguste
+/// @date December 2023
+/// @warning Cannot be used for pointers of dynamically-allocated array.
+class OmniPointer
+{
+private:
+    template <class T>
+    static void destroy_pointer(void* ptr)
+    {
+        if (ptr != nullptr)
+            delete reinterpret_cast<T*>(ptr);
+    }
+
+    void* _ptr;
+    std::size_t type_hash;
+    void(*destroyer)(void*);
+
+public:
+    /// @brief Construct an empty OmniPointer.
+    constexpr OmniPointer() noexcept : _ptr(nullptr), type_hash(0), destroyer(nullptr) {}
+
+    /// @brief Has the same effect as the default constructor.
+    constexpr OmniPointer(std::nullptr_t) noexcept : OmniPointer() {}
+
+    /// @brief Deleted. A copy mechanism of `OmniPointer` shall not be allowed.
+    OmniPointer(const OmniPointer&) = delete;
+
+    /// @brief Constructs an `OmniPointer` and moves in every information contained by the specified `OmniPointer`.
+    /// @param var The other `OmniPointer`.
+    OmniPointer(OmniPointer&&) noexcept;
+
+    /// @brief Constructs an OmniPointer from the given pointer.
+    /// @tparam T The type of the object the given pointer is pointing at.
+    /// @param pvar The given pointer.
+    /// @warning Assigning a local raw pointer is not recommended for it may cause dangling pointers in case two `OmniPointer`s are constructed from the same raw pointer or it is itself a dangling or wild pointer. Assigning a pointer of a dynamic array is also not recommended for the deletion mechanism will most likely be invalid.
+    template <class T>
+    OmniPointer(T* pvar) : _ptr((void*)pvar), type_hash(pvar == nullptr ? 0 : typeid(T).hash_code()), destroyer(destroy_pointer<T>) {}
+
+    /// @brief Gets the address of the object the `OmniPointer` is pointing to.
+    /// @tparam T The desired type
+    /// @return A `T` pointer containing the address the `OmniPointer` is pointing at.
+    /// @exception std::logic_error when `T` does not match the information (given by the hash code) stored in the `OmniPointer`.
+    template <class T>
+    T* Get() const
+    {
+        if (typeid(T).hash_code() == type_hash)
+            return reinterpret_cast<T*>(_ptr);
+        else
+            throw std::logic_error("The latest stored object is not of the requested type.");
+    }
+
+    /// @brief Releases the ownership of object, if any.
+    /// @tparam T The desired type
+    /// @return A `T` pointer containing the address the `OmniPointer` was previously pointing at.
+    /// @exception std::logic_error when `T` does not match the information (given by the hash code) stored in the `OmniPointer`.
+    /// @note A null pointer may be returned if the `OmniPointer` previously had no stored object.
+    template <class T>
+    T* Release()
+    {
+        if (std::is_void<T>::value || typeid(T).hash_code() == type_hash)
+        {
+            T* res = reinterpret_cast<T*>(_ptr);
+
+            _ptr = nullptr;
+            type_hash = 0;
+            destroyer = nullptr;
+
+            return res;
+        }
+        else
+            throw std::logic_error("The latest stored object is not of the requested type.");
+    }
+
+    /// @brief Resets the `OmniPointer` and changes the ownership of object if `tptr` is not null.
+    /// @tparam T The desired type
+    /// @param tptr The pointer to the object to be acquired ownership of
+    template <class T>
+    void Reset(T* tptr = (T*) nullptr)
+    {
+        void* old_ptr = _ptr;
+
+        _ptr = (void*)tptr;
+        type_hash = (tptr == nullptr ? 0 : typeid(T).hash_code());
+
+        if (destroyer != nullptr)
+            destroyer(old_ptr);
+
+        destroyer = destroy_pointer<T>;
+    }
+
+    /// @brief Resets the `OmniPointer`.
+    void Reset(std::nullptr_t) noexcept;
+
+    /// @brief Swaps the ownerships between the `OmniPointer` and the other.
+    /// @param other The other `OmniPointer` to swap ownerships with
+    void Swap(OmniPointer& other) noexcept;
+
+    /// @brief Resets the `OmniPointer` and moves in every information contained by the other `OmniPointer`.
+    /// @param var The other `OmniPointer`.
+    OmniPointer& operator=(OmniPointer&& var) noexcept;
+
+    /// @brief Deleted. A copy mechanism of `OmniPointer` shall not be allowed.
+    OmniPointer& operator=(const OmniPointer&) = delete;
+
+    /// @brief Has the same effect as `OmniPointer::Reset(std::nullptr_t)`.
+    OmniPointer& operator=(std::nullptr_t) noexcept;
+
+    /// @brief The same as `OmniPointer::Get<T>()`.
+    /// @tparam T The desired type
+    /// @exception std::logic_error when `T` does not match the information (given by the hash code) stored in the `OmniPointer`.
+    template <class T>
+    operator T* () const
+    {
+        return Get<T>();
+    }
+
+    /// @brief Destroys the `OmniPointer` and the stored object therein, if any.
+    ~OmniPointer() noexcept;
+};
